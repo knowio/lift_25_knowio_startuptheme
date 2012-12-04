4f083a9412dc8122d63e847696b61767bbe920d8
Byron Gibson <fbg111@gmail.com>
2012-12-04 02:08:25 -0800

    update

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -24,7 +24,7 @@ class Boot {
     // Build SiteMap
     val entries = List(
       Menu.i("Home") / "index", // the simple way to declare a menu
-      Menu.i("About") / "about",
+      //Menu.i("About") / "about",
       Menu.i("Blog") / "blog",
       Menu.i("Lab") / "lab",
       Menu.i("Contact") / "contact",
